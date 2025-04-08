02c5b3b2ffeb0daa41ade4bc3156108d27e48820
RebeccaFrem02 <155422197+RebeccaFrem02@users.noreply.github.com>
2025-04-08 13:37:25 +0300

    ..

--- src/Layout/Header/HeaderRight.tsx
+++ src/Layout/Header/HeaderRight.tsx
@@ -8,6 +8,7 @@ import MaximizeScreen from "./MaximizeScreen";
 
 export const HeaderRight = () => {
   return (
+    
     <Col xxl="8" xl="6" md="7" xs="8" className="nav-right pull-right right-header p-0 ms-auto">
       <ul className="nav-menus">
         <SearchBox />
