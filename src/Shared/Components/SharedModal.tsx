47ae1e498a07ba91ac54ca4cc44ce73999a02850
alihajhassn <Ali.HajjHassan@SoftMachine.co>
2025-04-24 16:06:27 +0300

    RemovedComment

--- src/Shared/Components/SharedModal.tsx
+++ src/Shared/Components/SharedModal.tsx
@@ -1,39 +1,3 @@
-// import React, { ReactNode } from "react";
-// import { Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
-
-// interface SharedModalProps {
-//   visible: boolean;
-//   onClose: () => void;
-//   title: string;
-//   children: ReactNode;
-//   footer: ReactNode;
-// }
-
-// const SharedModal: React.FC<SharedModalProps> = ({
-//   visible,
-//   onClose,
-//   title,
-//   children,
-//   footer,
-// }) => {
-//   return (
-//     <Modal
-//       isOpen={visible}
-//       toggle={onClose}
-//       className="modal-xxl modal-dialog-scrollable"
-//       backdrop="static"
-//       centered
-//     >
-//       <ModalHeader toggle={onClose}>{title}</ModalHeader>
-//       <ModalBody style={{ maxHeight: "70vh", overflowY: "auto" }}>
-//         {children}
-//       </ModalBody>
-//       <ModalFooter>{footer}</ModalFooter>
-//     </Modal>
-//   );
-// };
-
-// export default SharedModal;
 import React, { ReactNode } from "react";
 import { Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 
