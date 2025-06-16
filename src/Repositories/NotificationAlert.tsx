8834a4753d6bc882d6949d33abe0eeccac97f816
alihajhassn <165756058+alihajhassn@users.noreply.github.com>
2025-06-16 11:08:07 +0300

    Delete src/Repositories/NotificationAlert.tsx

--- src/Repositories/NotificationAlert.tsx
+++
@@ -1,9 +0,0 @@
-const service = "/api/NotificationAlert/";
-
-export const NotificationAlertRepository = {
-  NotificationTypes: {
-    getAll: service + "NotificationTypes/getAllTypes",
-    update: service + "NotificationTypes/updateType",
-    set: service + "NotificationTypes/setTypes",
-  },
-};
